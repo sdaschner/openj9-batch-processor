e7811278e2b6c0a293dd7e8683d4026a2a56a4bc
Sebastian Daschner <git@sebastian-daschner.de>
2019-05-15 07:39:24 +0300

    cleanup for demo

--- demos/openj9-jitaas/run_demo_sub.sh
+++
@@ -1,4 +0,0 @@
-#docker run --name openj9_jitaas_client_1 oj9d_jitaas_client:latest &
-docker run --name openj9_jitaas_client -v $(pwd)/batch-processor-0.0.1-SNAPSHOT.jar:/batch-processor-0.0.1-SNAPSHOT.jar raguks/ragustry:jitasv2 /jdk/bin/java -jar -XX:JITaaSClient:server=192.168.1.63 batch-processor-0.0.1-SNAPSHOT.jar --run-type=medium &
-
-docker run --name openj9_tuned --mount source=java-shared-classes,target=/cache oj9d_openj9_tuned_medium_run:latest &
